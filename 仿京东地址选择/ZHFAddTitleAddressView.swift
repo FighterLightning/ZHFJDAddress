521f2c0b33dec4eb8fe2135ce2110fd8529b5b96
张海峰 <991150443@qq.com>
2018-05-10 17:06:57 +0800

    Update ZHFAddTitleAddressView.swift

--- 仿京东地址选择/ZHFAddTitleAddressView.swift
+++ 仿京东地址选择/ZHFAddTitleAddressView.swift
@@ -5,7 +5,17 @@
 //  Created by 张海峰 on 2017/12/8.
 //  Copyright © 2017年 张海峰. All rights reserved.
 //
-
+//这是一个自定义仿京东地址选择器。Swift版本，（保证集成成功，有不懂的地方可加QQ：991150443 进行讨论。）
+OC版本地址：https://github.com/FighterLightning/ZHFJDAddressOC.git
+/*该demo的使用须知:
+ 1.下载该demo。把ProvinceModel.swift（必须），ZHFAddTitleAddressView.swift(必须) NetworkTools.swift(可用自己封装的)拖进项目
+ 2.pod 'Chrysan', :git => 'https://github.com/Harley-xk/Chrysan.git' //第三方加载框（根据需求进行添加）
+ pod 'AFNetworking'//网络请求
+ pod 'YYModel' //字典转模型
+ 3.把以下代码添加进自己的控制器方可使用，网络请求看ZHFAddTitleAddressView.swift头部注释根据需求进行修改
+ 4.如果感觉有帮助，不要吝啬你的星星哦！
+  该demo地址：https://github.com/FighterLightning/ZHFJDAddress.git
+ */
 /*
  这个视图你需要修改的地方为：
  func getAddressMessageData(addressID: NSInteger, provinceIdOrCityId: NSInteger)
